eb22aad9aa47be59049fcc93bae606799b027a64
John Jermanis <jermanis@yahoo.com>
2023-05-10 09:30:51 -0700

    Day 23 Optimization

--- AoC2016/AssembunnyVm.cs
+++ AoC2016/AssembunnyVm.cs
@@ -2,18 +2,22 @@
 
 internal class AssembunnyVm
 {
+    // TODO try making _instructions a member of all Instructions, to avoid having to pass repeatedly.
+    //  Or maybe Instructions all have a reference to AssembunnyVm, which would have needed members?
+    // TODO switch to using an array internally, convert to Dictionary at end
 
     internal interface Instruction
     {
-        int? Execute(Dictionary<char, int> registers);
+        int? Execute(Dictionary<char, int> registers, int pc, List<Instruction> instructions);
+        Instruction Toggle();
     }
 
-    internal class CpyFromValue : Object, Instruction
+    internal class CpyNumReg : Object, Instruction
     {
         private readonly int ValueSrc;
         private readonly char RegisterDest;
 
-        public CpyFromValue(int valueSrc, char regDest)
+        public CpyNumReg(int valueSrc, char regDest)
         {
             ValueSrc = valueSrc;
             RegisterDest = regDest;
@@ -22,19 +26,22 @@ internal class AssembunnyVm
         public override string ToString()
             => $"cpy {ValueSrc} {RegisterDest}";
 
-        public int? Execute(Dictionary<char, int> registers)
+        public int? Execute(Dictionary<char, int> registers, int pc, List<Instruction> instructions)
         {
             registers[RegisterDest] = ValueSrc;
             return null;
         }
+
+        public Instruction Toggle()
+            => new JmpNumReg(ValueSrc, RegisterDest);
     }
 
-    internal class CpyFromReg : Object, Instruction
+    internal class CpyRegReg : Object, Instruction
     {
         private readonly char RegisterSrc;
         private readonly char RegisterDest;
 
-        public CpyFromReg(char regSrc, char regDest)
+        public CpyRegReg(char regSrc, char regDest)
         {
             RegisterSrc = regSrc;
             RegisterDest = regDest;
@@ -43,12 +50,15 @@ internal class AssembunnyVm
         public override string ToString()
             => $"cpy {RegisterSrc} {RegisterDest}";
 
-        public int? Execute(Dictionary<char, int> registers)
+        public int? Execute(Dictionary<char, int> registers, int pc, List<Instruction> instructions)
         {
             registers[RegisterDest] = registers[RegisterSrc];
             return null;
         }
-    }
+
+        public Instruction Toggle()
+            => new JmpRegReg(RegisterSrc, RegisterDest);
+        }
 
     internal class Inc : Object, Instruction
     {
@@ -62,11 +72,14 @@ internal class AssembunnyVm
         public override string ToString()
             => $"inc {Register}";
 
-        public int? Execute(Dictionary<char, int> registers)
+        public int? Execute(Dictionary<char, int> registers, int pc, List<Instruction> instructions)
         {
             registers[Register]++;
             return null;
         }
+
+        public Instruction Toggle()
+            => new Dec(Register);
     }
 
     internal class Dec : Object, Instruction
@@ -81,11 +94,15 @@ internal class AssembunnyVm
         public override string ToString()
             => $"dec {Register}";
 
-        public int? Execute(Dictionary<char, int> registers)
+        public int? Execute(Dictionary<char, int> registers, int pc, List<Instruction> instructions)
         {
             registers[Register]--;
             return null;
         }
+
+        public Instruction Toggle()
+            => new Inc(Register);
+
     }
 
     internal class JmpRegNum : Object, Instruction
@@ -102,12 +119,42 @@ internal class AssembunnyVm
         public override string ToString()
             => $"jnz {RegCheck} {ValueDelta}";
 
-        public int? Execute(Dictionary<char, int> registers)
+        public int? Execute(Dictionary<char, int> registers, int pc, List<Instruction> instructions)
         {
             if (registers[RegCheck] != 0)
                 return ValueDelta;
             return null;
         }
+
+        public Instruction Toggle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class JmpNumReg : Object, Instruction
+    {
+        private readonly int ValueCheck;
+        private readonly char RegDelta;
+
+        public JmpNumReg(int valueCheck, char regDelta)
+        {
+            ValueCheck = valueCheck;
+            RegDelta = regDelta;
+        }
+
+        public override string ToString()
+            => $"jnz {ValueCheck} {RegDelta}";
+
+        public int? Execute(Dictionary<char, int> registers, int pc, List<Instruction> instructions)
+        {
+            if (ValueCheck != 0)
+                return registers[RegDelta];
+            return null;
+        }
+
+        public Instruction Toggle()
+            => new CpyNumReg(ValueCheck, RegDelta);
     }
 
     internal class JmpNumNum : Object, Instruction
@@ -124,11 +171,72 @@ internal class AssembunnyVm
         public override string ToString()
             => $"jnz {ValueCheck} {ValueDelta}";
 
-        public int? Execute(Dictionary<char, int> registers)
+        public int? Execute(Dictionary<char, int> registers, int pc, List<Instruction> instructions)
         {
             if (ValueCheck != 0)
                 return ValueDelta;
             return null;
+        }
+
+        public Instruction Toggle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class JmpRegReg : Object, Instruction
+    {
+        private readonly char RegCheck;
+        private readonly char RegDelta;
+
+        public JmpRegReg(char regCheck, char regDelta)
+        {
+            RegCheck = regCheck;
+            RegDelta = regDelta;
+        }
+
+        public override string ToString()
+            => $"jnz {RegCheck} {RegDelta}";
+
+        public int? Execute(Dictionary<char, int> registers, int pc, List<Instruction> instructions)
+        {
+            if (registers[RegCheck] != 0)
+                return registers[RegDelta];
+            return null;
+        }
+
+        public Instruction Toggle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Tgl : Object, Instruction
+    {
+        private readonly char Register;
+
+        public Tgl(char register)
+        {
+            Register = register;
+        }
+
+        public override string ToString()
+            => $"tcl {Register}";
+
+        public int? Execute(Dictionary<char, int> registers, int pc, List<Instruction> instructions)
+        {
+            var dest = pc + registers[Register];
+            if (dest < instructions.Count)
+            {
+                var newInst = instructions[dest].Toggle();
+                instructions[dest] = newInst;
+            }
+            return null;
+        }
+
+        public Instruction Toggle()
+        {
+            return new Inc(Register);
         }
     }
 
@@ -150,9 +258,9 @@ internal class AssembunnyVm
                 {
                     var isVal = int.TryParse(tokens[1], out int value);
                     if (isVal)
-                        return new CpyFromValue(value, tokens[2][0]);
+                        return new CpyNumReg(value, tokens[2][0]);
                     else
-                        return new CpyFromReg(tokens[1][0], tokens[2][0]);
+                        return new CpyRegReg(tokens[1][0], tokens[2][0]);
                 }
             case "inc":
                 return new Inc(tokens[1][0]);
@@ -161,12 +269,24 @@ internal class AssembunnyVm
             case "jnz":
                 {
                     var isVal1 = int.TryParse(tokens[1], out int value1);
-                    var value2 = int.Parse(tokens[2]);
+                    var isVal2 = int.TryParse(tokens[2], out int value2);
                     if (isVal1)
-                        return new JmpNumNum(value1, value2);
+                    {
+                        if (isVal2)
+                            return new JmpNumNum(value1, value2);
+                        else
+                            return new JmpNumReg(value1, tokens[2][0]);
+                    }
                     else
-                        return new JmpRegNum(tokens[1][0], value2);
+                    {
+                        if (isVal2)
+                            return new JmpRegNum(tokens[1][0], value2);
+                        else
+                            return new JmpRegReg(tokens[1][0], tokens[2][0]);
+                    }
                 }
+            case "tgl":
+                return new Tgl(tokens[1][0]);
             default:
                 throw new Exception($"Unhandled: {line}");
         }
@@ -176,7 +296,7 @@ internal class AssembunnyVm
     {
         for (var pc = 0; pc < _instructions.Count(); pc++)
         {
-            var delta = _instructions[pc].Execute(registers);
+            var delta = _instructions[pc].Execute(registers, pc, _instructions);
             if (delta != null)
                 pc += delta.Value - 1;
         }
--- AoC2016/Day23.cs
+++ AoC2016/Day23.cs
@@ -2,16 +2,15 @@
 
 public class Day23 : DayBase, IDay
 {
-    // TODO this is described to be based on the work from Day 12. Refactor
-    // that code to be shared?
-    // TODO, it takes 6.5 minutes to run 23-2. Proposed optimization from Day
-    // 12 would help.
+    // TODO, it takes 1.5 minutes to run 23-2. Already optimized. Look for more?
     // TODO text of AoC trying multiplication to replace inc and dec.
- 
+
     private readonly IList<string> _lines;
 
     public Day23(string filename)
-        => _lines = TextFileStringList(filename);
+    {
+        _lines = TextFileStringList(filename);
+    }
 
     public Day23() : this("Day23.txt")
     {
@@ -24,19 +23,17 @@ public class Day23 : DayBase, IDay
     }
 
     public int Value7Eggs()
-    {
-        var registers = InitRegisters();
-        registers['a'] = 7;
-        RunProgram(registers);
-        return registers['a'];
-    }
+        => ValueForSafe(7);
 
     public int Value12Eggs()
-    {
+        => ValueForSafe(12);
 
+    private int ValueForSafe(int eggCount)
+    {
+        var vm = new AssembunnyVm(_lines);
         var registers = InitRegisters();
-        registers['a'] = 12;
-        RunProgram(registers);
+        registers['a'] = eggCount;
+        registers = vm.RunProgram(registers);
         return registers['a'];
     }
 
@@ -48,59 +45,4 @@ public class Day23 : DayBase, IDay
         return result;
     }
 
-    private int ParseValue(string val, Dictionary<char, int> registers)
-    {
-        if (int.TryParse(val, out var valNum))
-            return valNum;
-        else
-            return registers[val[0]];
-    }
-
-    private void RunProgram(Dictionary<char, int> registers)
-    {
-        var program = new List<string>(_lines);
-        for (var i = 0; i < program.Count(); i++)
-        {
-            var curr = program[i].Split(' ');
-            var inst = curr[0];
-
-            switch (inst)
-            {
-                case "cpy":
-                    registers[curr[2][0]] = ParseValue(curr[1], registers);
-                    break;
-                case "inc":
-                    registers[curr[1][0]]++;
-                    break;
-                case "dec":
-                    registers[curr[1][0]]--;
-                    break;
-                case "jnz":
-                    if (ParseValue(curr[1], registers) != 0)
-                        i += ParseValue(curr[2], registers) - 1;
-                    break;
-                case "tgl":
-                    var dist = registers[curr[1][0]];
-                    var targetAddr = i + dist;
-                    if (targetAddr < program.Count())
-                    {
-                        var targetInst = program[targetAddr].Split(' ');
-                        string newInst;
-                        if (targetInst.Length == 2)
-                        {
-                            newInst = targetInst[0].Equals("inc") ? "dec" : "inc";
-                            program[targetAddr] = $"{newInst} {targetInst[1]}";
-                        }
-                        else
-                        {
-                            newInst = targetInst[0].Equals("jnz") ? "cpy" : "jnz";
-                            program[targetAddr] = $"{newInst} {targetInst[1]} {targetInst[2]}";
-                        }
-                    }
-                    break;
-                default:
-                    throw new Exception($"Unknown instruction: {inst}");
-            }
-        }
-    }
 }
